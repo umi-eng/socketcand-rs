773ac707dda1e95e556462d800ff5795ec9132d0
Liam Kinne <liam@kinne.io>
2024-07-01 22:35:57 +1000

    add test python script

---
+++ examples/print.py
@@ -0,0 +1,10 @@
+import can
+
+bus = can.interface.Bus(interface='socketcand', host="192.168.0.16", port=29536, channel="can0")
+
+try:
+  while True:
+    msg = bus.recv()
+    print(msg)
+except KeyboardInterrupt:
+  pass
